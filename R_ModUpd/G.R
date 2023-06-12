dbf8901ead1408ab2d70925039c5f51857d04243
alej <alex.diaz@ucl.ac.uk>
2023-06-12 20:21:20 +0100

    G.R now loads theta and calls bnn

--- R_ModUpd/G.R
+++ R_ModUpd/G.R
@@ -2,6 +2,8 @@
 #==============================================================================
 G <- function(X){
   #G.y <- identif_y(X) # CBUS paper example 
-  G.y <- bnn(X) # CBUS paper example 
+  
+  theta.true <- c( alpha1=5,alpha2=-5,beta1=-1,beta2=-3,gamma1=5,gamma2=2,sigma_eps=.1 )
+  G.y <- bnn(X,theta.true) # CBUS paper example 
   return(G.y)
 }
